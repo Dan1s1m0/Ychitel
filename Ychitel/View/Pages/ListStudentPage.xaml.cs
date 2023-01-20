aa7a0403f2b963a6d22ef741a3f26136ce5ed7d5
rotoyouoi <rotoyouoi@yandex.ru>
2023-01-20 11:06:54 +0500

    123

--- Ychitel/View/Pages/ListStudentPage.xaml.cs
+++ Ychitel/View/Pages/ListStudentPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Ychitel.Model;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace Ychitel.View.Pages
 {
@@ -20,10 +22,77 @@ namespace Ychitel.View.Pages
     /// </summary>
     public partial class ListStudentPage : Page
     {
+        Core db = new Core();
+        
         public ListStudentPage()
         {
             InitializeComponent();
+            ProductDataGrid.ItemsSource = db.context.Students.ToList();
+        }
 
+        private void ButtonWordTextBoxClick(object sender, RoutedEventArgs e)
+        {
+            Word.Application application = new Word.Application();
+            Word.Document document = application.Documents.Add();
+            
+            Word.Paragraph titleparagraph = document.Paragraphs.Add();
+            Word.Range titleRange = titleparagraph.Range;
+            titleRange.Text = " МИНИСТЕРСТВО ОБРАЗОВАНИЯ И МОЛОДЕЖНОЙ ПОЛИТИКИ СВЕРДЛОВСКОЙ ОБЛАСТИ ";
+            titleRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            titleRange.InsertParagraphAfter();
+            titleparagraph = document.Paragraphs.Add();
+            titleRange = titleparagraph.Range;
+            titleRange.Text = " ГОСУДАРСТВЕННОЕ АВТОНОМНОЕ ПРОФЕССИОНАЛЬНОЕ ОБРАЗОВАТЕЛЬНОЕ УЧРЕЖДЕНИЕ  СВЕРДЛОВСКОЙ ОБЛАСТИ ";
+            titleRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            titleRange.InsertParagraphAfter();
+            
+            titleparagraph = document.Paragraphs.Add();
+            titleRange = titleparagraph.Range;
+            titleRange.Text = " «ЕКАТЕРИНБУРГСКИЙ МОНТАЖНЫЙ КОЛЛЕДЖ» ";
+            titleRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            titleRange.Bold = 1;
+            titleRange.InsertParagraphAfter();
+            
+            titleparagraph = document.Paragraphs.Add();
+            titleRange = titleparagraph.Range;
+            titleRange.Text = " ВЕДОМОСТЬ";
+            titleRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            titleRange.Bold = 1;
+            titleRange.InsertParagraphAfter();
+            
+            titleparagraph = document.Paragraphs.Add();
+            titleRange = titleparagraph.Range;
+            titleRange.Text = "итоговой аттестации";
+            titleRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            titleRange.Bold = 1;
+            titleRange.InsertParagraphAfter();
+
+            Word.Paragraph tableParagraph = document.Paragraphs.Add();
+            Word.Range tableRange = tableParagraph.Range;
+            Word.Table titleTable = document.Tables.Add(tableRange,1,3);
+            Word.Range cellRange;
+            cellRange = titleTable.Cell(1, 1).Range;
+            cellRange.Text = "«_____» _________ 20_____ Г.";
+            cellRange = titleTable.Cell(1, 3).Range;
+            cellRange.Text = "№_______________________";
+            titleRange.InsertParagraphAfter();
+
+
+            titleparagraph = document.Paragraphs.Add();
+            titleRange = titleparagraph.Range;
+            titleRange.Text = "Группа №: ";
+            titleRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            titleRange.InsertParagraphAfter();
+            
+            titleparagraph = document.Paragraphs.Add();
+            titleRange = titleparagraph.Range;
+            titleRange.Text = "Преподаватель:";
+            titleRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            titleRange.InsertParagraphAfter();
+
+
+
+            application.Visible = true;
         }
     }
 }
--- Ychitel/View/Pages/ListTeacherPage.xaml.cs
+++ Ychitel/View/Pages/ListTeacherPage.xaml.cs
@@ -12,24 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Ychitel.View.Pages;
 
-namespace Ychitel
+namespace Ychitel.View.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для ListTeacherPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ListTeacherPage : Page
     {
-        public MainWindow()
+        public ListTeacherPage()
         {
             InitializeComponent();
-            MainFrame.Navigate(new AuthPages());
-        }
-
-        private void BackButtonClick(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
